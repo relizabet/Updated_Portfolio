18e10d4a49e1b7c61baae7e819971dd62730a5d3
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-12 13:57:34 -0400

    contact form attempt

---
+++ form.php
@@ -0,0 +1,13 @@
+<!-- contact form php code taken from https://1stwebdesigner.com/php-contact-form-html/ -->
+
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "rach.eliza.campbell@icloud.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
+

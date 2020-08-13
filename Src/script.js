cbdb1d14be43caa6ecc81ac087351c32701b52d9
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-13 14:24:51 -0400

    portfolio links

--- Src/script.js
+++ Src/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-  console.log("hello");
-});
+"use strict";
+
+console.log("ready");
+
+// let test = document.querySelector(".test");
+
+// test.addEventListener(
+//   "mouseover",
+//   function (event) {
+//     event.target.style.color = "orange";
+
+//     setTimeout(function () {
+//       event.target.style.color = "";
+//     }, 500);
+//   },
+//   false
+// );
